73d007ad4d799a7265c460ff17df5999e06b80d8
OEbinisa <o.ebinisa@gmail.com>
2024-11-11 23:31:31 +0100

    app + ci-actions + jest 10

---
+++ client/babel-jest.config.js
@@ -0,0 +1,5 @@
+// babel-jest.config.js
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/transform-runtime", "babel-plugin-transform-import-meta"],
+};
